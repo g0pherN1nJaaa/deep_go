7559e4effbad8a8f158399ee78aa5ba9ab75b4d7
vladislav.v13 <vladislav.v13@wb.ru>
2025-05-04 23:20:49 +0300

    Module 1

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -9,10 +9,7 @@ import (
 // go test -v homework_test.go
 
 func ToLittleEndian(number uint32) uint32 {
-	return ((number << 24) & 0xFF000000) |
-		((number << 8) & 0x00FF0000) |
-		((number >> 8) & 0x0000FF00) |
-		((number >> 24) & 0x000000FF)
+	return 0 // need to implement
 }
 
 func TestСonversion(t *testing.T) {
