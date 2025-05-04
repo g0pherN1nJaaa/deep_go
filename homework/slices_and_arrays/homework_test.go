3cd45567bdbc26571be1cd28a9bb5bad091938fa
vladislav.v13 <vladislav.v13@wb.ru>
2025-05-04 23:37:25 +0300

    hwk2

--- homework/slices_and_arrays/homework_test.go
+++ homework/slices_and_arrays/homework_test.go
@@ -10,36 +10,100 @@ import (
 // go test -v homework_test.go
 
 type CircularQueue struct {
-	values []int
+	values         []int
+	idxLastElement int
+	beginIdx       int
+	numElem        int
 	// need to implement
 }
 
 func NewCircularQueue(size int) CircularQueue {
-	return CircularQueue{} // need to implement
+	return CircularQueue{
+		values:         make([]int, size),
+		idxLastElement: size - 1,
+		beginIdx:       0,
+		numElem:        0,
+	} // need to implement
 }
 
 func (q *CircularQueue) Push(value int) bool {
-	return false // need to implement
+	if q.Full() {
+		return false
+	}
+
+	firstNotEngagedIndex := q.sumIndex(q.beginIdx, q.numElem)
+	q.values[firstNotEngagedIndex] = value
+
+	q.numElem++
+
+	return true
 }
 
 func (q *CircularQueue) Pop() bool {
-	return false // need to implement
+	if q.Empty() {
+		return false
+	}
+
+	q.beginIdx = q.incrementIndex(q.beginIdx)
+
+	q.numElem--
+
+	return true
 }
 
 func (q *CircularQueue) Front() int {
-	return -1 // need to implement
+	if q.Empty() {
+		return -1
+	}
+
+	return q.values[q.beginIdx]
 }
 
 func (q *CircularQueue) Back() int {
-	return -1 // need to implement
+	if q.Empty() {
+		return -1
+	}
+
+	firstIdxEmpty := q.sumIndex(q.beginIdx, q.numElem)
+	return q.values[q.decrementIndex(firstIdxEmpty)]
 }
 
 func (q *CircularQueue) Empty() bool {
-	return false // need to implement
+	return q.numElem == 0
 }
 
 func (q *CircularQueue) Full() bool {
-	return false // need to implement
+	return q.numElem == len(q.values)
+}
+
+func (q *CircularQueue) incrementIndex(index int) int {
+	index++
+
+	if index > q.idxLastElement {
+		return 0
+	}
+
+	return index
+}
+
+func (q *CircularQueue) decrementIndex(index int) int {
+	index--
+
+	if index < 0 {
+		return q.idxLastElement
+	}
+
+	return index
+}
+
+func (q *CircularQueue) sumIndex(index int, valueToAdd int) int {
+	index += (valueToAdd % len(q.values))
+
+	if index > q.idxLastElement {
+		return index - len(q.values)
+	}
+
+	return index
 }
 
 func TestCircularQueue(t *testing.T) {
